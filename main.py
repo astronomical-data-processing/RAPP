18e76791a97283c49ca8bdc045b51de47d235d2d
xie-zhou <xiezhou@cnlab.net>
2019-11-02 16:39:57 +0800

    forgot change main.py

--- main.py
+++ main.py
@@ -17,36 +17,17 @@ from matplotlib import pyplot as plt
 from scipy import ndimage as nd
 
 
-def run1():
-    app = core.APpipeline(data=r'/media/xiezhou/_dde_data/data/rawdata/3C454/i',
-                          #   bias=r'/media/xiezhou/_dde_data/data/rawdata/bias/g',
-                          #   dark=r'/media/xiezhou/_dde_data/data/rawdata/dark/g',
-                          #   flat=r'/media/xiezhou/_dde_data/data/rawdata/twflat/g',
+def run():
+    app = core.APpipeline(data=r'data/',
+                          bias=r'bias/',
+                          dark=r'dark/',
+                          flat=r'flat/',
                           expo_key='EXPOS',
                           date_key='DATE',)
     app.info_init()
     app.match()
     img = app.img_combine()
-    info0 = app.find_star(img, True, 7)
-    app.match(info0)
-    app.ap(info0)
-    app.draw(folder=folder,
-             img_ref=img,
-             info0=info0)
-    app.save(folder=folder)
-
-
-def run2():
-    app = core.APpipeline(data=r'/media/xiezhou/_dde_data/data/rawdata/3C454/i',
-                          bias=r'/media/xiezhou/_dde_data/data/rawdata/bias/i',
-                          dark=r'/media/xiezhou/_dde_data/data/rawdata/dark/i',
-                          flat=r'/media/xiezhou/_dde_data/data/rawdata/twflat/i',
-                          expo_key='EXPOS',
-                          date_key='DATE',)
-    app.info_init()
-    app.match()
-    img = app.img_combine()
-    info0 = app.find_star(img, True, 7)
+    info0 = app.find_star(img, True, 10)
     app.match(info0)
     app.ap(info0)
     app.draw(folder=folder,
@@ -57,6 +38,4 @@ def run2():
 
 if __name__ == "__main__":
     folder = 'local/result1/'
-    run1()
-    folder = 'local/result2/'
-    run2()
+    run()
